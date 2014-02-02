d9fa2e1bfbfb9204b6d839cdd803e8f51ec2c36d
Ben Biddington <ben.biddington@gmail.com>
2014-02-02 14:13:11 +1300

    You just have to wait for the server to start....

--- spec/system.tests/drb_observers_spec.rb
+++ spec/system.tests/drb_observers_spec.rb
@@ -5,20 +5,40 @@ describe "Basic drb connections" do
     exe = File.join(File.dirname(__FILE__), "bin", "server")
     
     @pid = Process.spawn "bundle exec ruby #{exe}"
-  end
 
-  after :all do
-    Process.kill("HUP", @pid)
-    Process.wait
-  end
+    puts @pid
 
-  it "tells me the time" do
+    sleep 5
+
     require 'drb/drb'
 
     DRb.start_service
 
-    timeserver = DRbObject.new_with_uri "druby://127.0.0.1:9999"
+    @timeserver = DRbObject.new_with_uri "druby://127.0.0.1:9999"
+  end
 
-    expect(timeserver.get_current_time).to_not be_nil
+  after :all do
+    Process.kill("SIGKILL", @pid)
+    Process.wait
+  end
+
+  it "tells me the time" do
+    expect(@timeserver.get_current_time.to_s).to match /^#{Time.now.year}/
+  end
+
+  it "can notify" do
+    an_observer = Class.new do
+      def update(args)
+        @notified = true
+      end
+      
+      def notified?; @notified === true; end
+    end.new
+    
+    @timeserver.add_observer an_observer
+
+    @timeserver.progress
+    
+    expect(an_observer).to be_notified
   end
 end
