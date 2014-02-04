4f0cb3556a09a01b6103e3eea672b8c7f13f77ff
Ben Biddington <ben.biddington@gmail.com>
2014-02-04 13:57:40 +1300

    can detach observer which stops notifications

--- spec/system.tests/drb/drb_observers_spec.rb
+++ spec/system.tests/drb/drb_observers_spec.rb
@@ -37,24 +37,44 @@ describe "Basic drb connections" do
     @server.kill
   end
 
-  it "tells me the time" do
-    expect(@timeserver.get_current_time.to_s).to match /^#{Time.now.year}/
-  end
-
-  it "can notify" do
-    an_observer = Class.new do
+  before do
+    @an_observer = Class.new do
       def update(args)
         @notified = true
       end
       
       def notified?; @notified === true; end
+
+      def reset; @notified = false; end
     end.new
-    
-    @timeserver.add_observer an_observer
+  end
+
+  it "tells me the time" do
+    expect(@timeserver.get_current_time.to_s).to match /^#{Time.now.year}/
+  end
+
+  it "can notify" do
+    @timeserver.add_observer @an_observer
 
     @timeserver.progress
     
-    expect(an_observer).to be_notified
+    expect(@an_observer).to be_notified
+  end
+
+  it "can detach observer which stops notifications" do
+    @timeserver.add_observer @an_observer
+
+    @timeserver.progress
+    
+    expect(@an_observer).to be_notified
+
+    @an_observer.reset
+
+    @timeserver.delete_observer @an_observer
+
+    @timeserver.progress
+    
+    expect(@an_observer).to_not be_notified
   end
 
   it "can also get audible notifications" do
